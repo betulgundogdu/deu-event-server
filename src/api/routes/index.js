534b08ee517c2fa5febb8b9fc4f2de0173678ed5
Betul Cukuren <b.cukuren@emakina.com.tr>
2022-02-03 21:46:15 +0300

    sdsddsf

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -17,7 +17,7 @@ router.post("/users/login", async (req, res) => {
             email: req.body.email,
             password: bcrypt.hashSync(req.body.password, 8)
         })
-        res.send('sdsdasd');
+        res.send(bcrypt.hashSync(req.body.password, 8));
     } catch (error) {
         res.send(error);
     }
